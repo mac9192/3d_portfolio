9d2f7e5c80af2b63f301b0b5b176341812f0d0b4
Miguel Colmenares <miguelcolmenares96@gmail.com>
2024-01-09 14:59:44 -0500

    About Page Updated

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -6,7 +6,7 @@ const About = () => {
 
     return(
        <section className="max-container"> 
-            <h1 className="head-text">Hello, I'm <span className="blue-gradient_text font-semibold drop-shadow">Miguel</span></h1>
+            <h1 className="head-text">Hello, Im <span className="blue-gradient_text font-semibold drop-shadow">Miguel</span></h1>
             <div className="mt-5 flex flex-col gap-3 text-slate-500">
                 <p> Software Engineer based in Atlanta, Ga. Specializing  in full stack web development.
 
@@ -15,9 +15,9 @@ const About = () => {
             <div className="py-10 flex flex-col">
                 <h3 className="subhead-text">My Skills</h3>
                 <div className="mt-16 flex flex-wrap gap-12">
-                    {skills.map((skill) => {
+                    {skills.map((skill, index) => {
                         return (
-                            <div className="block-container w-20 h-20">
+                            <div key={index}  className="block-container w-20 h-20">
                                 <div className="btn-back rounded-xl" />
                                 <div className="btn-front rounded-xl flex justify-center items-center">
                                     <Image className="w-1/2 h-1/2 object-contain" src={skill.imageUrl} alt={skill.name}/>
@@ -36,10 +36,10 @@ const About = () => {
                 </div>
                 <div className="mt-12 flex">
          
-                        {experiences.map((experience) => {
+                        {experiences.map((experience, index) => {
                             return (
                              
-                                    <div>
+                                    <div key={index}>
                                         <h3>
                                             {experience.title}
                                         </h3>
