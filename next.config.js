2633c03bc5908dc613350b4fd249989993d7708e
Miguel Colmenares <miguelcolmenares96@gmail.com>
2024-01-10 15:37:12 -0500

    Updated next.config

--- next.config.js
+++ next.config.js
@@ -15,25 +15,6 @@ module.exports = {
       },
     });
 
-    // Add loader for audio files (ogg, mp3, wav, mpeg)
-    config.module.rules.push({
-      test: /\.(ogg|mp3|wav|mpe?g)$/i,
-      exclude: config.exclude,
-      use: [
-        {
-          loader: require.resolve('url-loader'),
-          options: {
-            limit: config.inlineImageLimit,
-            fallback: require.resolve('file-loader'),
-            publicPath: `${config.assetPrefix}/_next/static/images/`,
-            outputPath: `${isServer ? '../' : ''}static/images/`,
-            name: '[name]-[hash].[ext]',
-            esModule: config.esModule || false,
-          },
-        },
-      ],
-    });
-
     return config;
   },
   sassOptions: {
